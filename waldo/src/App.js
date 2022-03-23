b239e1c42901d563b6f2f925b1ec3037d062d8d9
Shawn Lentz <shawn.lentz@gmail.com>
2022-03-23 13:44:33 -1000

    starting testing tagging logic

--- waldo/src/App.js
+++ waldo/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import style from './style.css';
+import Postcard from './images/postcard.jpeg'
 
+const WALDO = [500, 120]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function generateSquare(coordinates) {
+    // Given top left coordinates, ie [0,0], generate 100x100 pixel square
+    let square = 'Square'
+    return square
+  }
+
+  function sumTest(a, b) {
+    return a + b
+  }
+  function checkClick(coordinates) {
+    const square = generateSquare(coordinates)
+    console.log("Checking click", square)
+    // Return true if a click is within the given coordinates
+  }
+  function checkMouseClick(event) {
+    // return the event for a mouse click
+    // event.nativeEvent.offsetX/Y are the image coordinates
+    event.stopPropagation()
+    // console.log("Clicking div", event)
+    // console.log("Offset left", event.nativeEvent)
+    // let x = event.nativeEvent.offsetX;
+    // let y = event.nativeEvent.offsetY;
+    // let totX = event.clientX;
+    // let totY = event.clientY;
+    // console.log("Coordinates of image", x, y)
+    // console.log("Coordinates in page", totX, totY, event)
+    checkClick()
+  }
+
+  // return (
+  //   <div onClick={checkMouseClick}>
+  //     <h1>App loaded!</h1>
+  //     <img src={Postcard} onClick={checkMouseClick}/>
+  //   </div>
+  // )
 }
 
-export default App;
+function sum(a, b) {
+  return a + b
+}
+module.exports = sum;
+// export default App;
+
+
+// Starting from the top left corning x/y, create a 100/100 square
+// so if topleft = 500/120, then dimensions are 600/120, 500/220, 600/220
+// Top left 500/120
+// Top Right 550/120
+// Lower Left 500/175
+// Lower Right 550/175
+
+// Given the top left coordinates, generate a square 100x100 pixels wide
+
+// Using this square, determine if a click is within that square
--- waldo/src/index.js
+++ waldo/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Stack overflow for server-side click locations
+// https://stackoverflow.com/questions/34867066/javascript-mouse-click-coordinates-for-image
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
